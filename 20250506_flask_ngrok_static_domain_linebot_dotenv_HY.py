7816754dfbcebc682eb22b22be9bb96f8bec6fbe
hychou <hychou33server@gmail.com>
2025-05-06 15:32:59 +0800

    add/modify the comments

--- 20250506_flask_ngrok_static_domain_linebot_dotenv_HY.py
+++ 20250506_flask_ngrok_static_domain_linebot_dotenv_HY.py
@@ -20,35 +20,35 @@ from linebot.v3.webhooks import (
     TextMessageContent
 )
 
-# 載入 .env 檔案
+# load .env file
 load_dotenv()
 
-# 從環境變數取得 Token 和 Secret
+# 從.env file取得 Token 和 Secret
 LINE_ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 
 app = Flask(__name__)
 
-# 替换为你自己的 Channel Access Token 和 Channel Secret
+# Channel Access Token 和 Channel Secret
 configuration = Configuration(access_token=LINE_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# 处理根路径 "/" (用於檢測flask server是否有順利運作)
+# 處理根路徑 "/" (用於檢測flask server是否有順利運作)
 @app.route("/")
 def index():
     return "Hello, Flask is running!"
 
-# 处理LINE Bot 的 Webhook 回调路由
+# 處理LINE Bot的Webhook回调路由
 @app.route("/callback", methods=['POST'])
 def callback():
-    # 获取 X-Line-Signature 头部信息
+    # 取得 X-Line-Signature headers信息
     signature = request.headers['X-Line-Signature']
 
-    # 获取请求体数据
+    # 取得request body數據
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # 验证签名并处理 Webhook 请求
+    # 驗證簽名並處理 Webhook 請求
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -57,20 +57,20 @@ def callback():
 
     return 'OK'
 
-# 定义消息处理函数
+# 輸入訊息處理function
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    # 使用 ApiClient 建立与 LINE Messaging API 的连接
+    # 使用 ApiClient 建立與 LINE Messaging API 的連接
     with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)# 创建 MessagingApi 实例
-        # 创建 ReplyMessageRequest，用于发送回复消息
+        line_bot_api = MessagingApi(api_client)# 創建 MessagingApi 實例
+        # 創建 ReplyMessageRequest，用於發送回覆訊息
         reply_message_request = ReplyMessageRequest(
-            reply_token=event.reply_token,          # 使用事件的回复令牌
-            messages=[TextMessage(text=event.message.text)]  # 将接收到的消息文本作为回复
+            reply_token=event.reply_token,          # 使用事件的回覆令牌
+            messages=[TextMessage(text=event.message.text)]  # 將接收到的輸入訊息文本作為回覆訊息
         )
         
-        # 使用 MessagingApi 的 reply_message_with_http_info 方法发送消息
+        # 使用 MessagingApi 的 reply_message_with_http_info 方法發送訊息
         line_bot_api.reply_message_with_http_info(reply_message_request)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000) #host='0,0,0,0',允許ngrok無受限IP的訪問; port=5000, 須與ngork的port=5000一樣
+    app.run(host='0.0.0.0', port=5000) #host='0,0,0,0',允許ngrok無受限IP的訪問; port=5000, 須與ngrok的port=5000一樣
